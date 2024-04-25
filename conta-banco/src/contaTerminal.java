e5debe68847dbb60b30b9ab3412b188ea715354e
Theuus442 <teuzinggs@gmail.com>
2024-04-25 20:48:06 -0300

    Sistema de conta_Banco feito

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class contaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá, mundo!");
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("**************");
+        System.out.println("Conta bancária");
+        System.out.println("**************");
+
+        System.out.println("Por favor, informe seu nome: ");
+        String nomeCliente = sc.nextLine();
+        System.out.println("Inforome sua agência: ");
+        String agencia = sc.nextLine();
+        System.out.println("Seu número: ");
+        int numero = sc.nextInt();
+        System.out.println("Informe seu saldo atual: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo é de " + saldo + 
+        " já está disponível para saque!");
+        
+
+
     }
 }
